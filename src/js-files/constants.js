155ff182baebe97f000ff563ad665ec5c2602700
ottotuhkunen <otto.tuhkunen@hotmail.com>
2024-07-10 03:22:04 +0300

    fixing

--- src/js-files/constants.js
+++ src/js-files/constants.js
@@ -1,2 +1,2 @@
 export const mapboxToken = 'pk.eyJ1Ijoib3R0b3R1aGt1bmVuIiwiYSI6ImNseG41dW9vaDAwNzQycXNleWI1MmowbHcifQ.1ZMRPeOQ7z9GRzKILnFNAQ';
-export const updateInterval = 4000;
+export const updateInterval = 5000;
